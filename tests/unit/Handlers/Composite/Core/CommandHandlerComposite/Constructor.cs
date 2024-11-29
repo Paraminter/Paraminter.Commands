9045789f984a2b5807842b82b111c69134497192
ErikWe <erikwestberg7@gmail.com>
2024-11-29 19:30:03 +0100

    Added composite cmd handler

---
+++ tests/unit/Handlers/Composite/Core/CommandHandlerComposite/Constructor.cs
@@ -0,0 +1,33 @@
+﻿namespace Paraminter.Cqs.Composite;
+
+using Moq;
+
+using System;
+using System.Collections.Generic;
+
+using Xunit;
+
+public sealed class Constructor
+{
+    [Fact]
+    public void NullComponents_ThrowsArgumentNullException()
+    {
+        var result = Record.Exception(() => Target<ICommand>(null!));
+
+        Assert.IsType<ArgumentNullException>(result);
+    }
+
+    [Fact]
+    public void ValidArguments_ReturnsHandler()
+    {
+        var result = Target(Mock.Of<IEnumerable<ICommandHandler<ICommand>>>());
+
+        Assert.NotNull(result);
+    }
+
+    private static CommandHandlerComposite<TCommand> Target<TCommand>(IEnumerable<ICommandHandler<TCommand>> components)
+        where TCommand : ICommand
+    {
+        return new CommandHandlerComposite<TCommand>(components);
+    }
+}
---
+++ tests/unit/Handlers/Composite/Core/CommandHandlerComposite/IFixture.cs
@@ -0,0 +1,13 @@
+﻿namespace Paraminter.Cqs.Composite;
+
+using Moq;
+
+using System.Collections.Generic;
+
+internal interface IFixture<TCommand>
+    where TCommand : ICommand
+{
+    public abstract ICommandHandler<TCommand> Sut { get; }
+
+    public abstract Mock<IEnumerable<ICommandHandler<TCommand>>> ComponentsMock { get; }
+}
